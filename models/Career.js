7262c7124451748d3414a2ee93b249b39e3faecf
tgyeom <ktg5679@gmail.com>
2024-05-21 11:12:14 +0900

    feat: career (경력) 모델 작성 완료

---
+++ models/Career.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/Database");
+const Article = require("./Article");
+const User = require("./Users");
+const Qreply = require("./Qreply");
+
+const Career = sequelize.define("Career", {
+  career_no: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user_no: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    references: {
+      model: User,
+      key: "user_no",
+    },
+  },
+  career_name: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  },
+  career_startdate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  career_enddate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
+
+module.exports = Career;
