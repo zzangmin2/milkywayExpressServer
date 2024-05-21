865d25d367e6173b2ed703df62b264284ce85772
tgyeom <ktg5679@gmail.com>
2024-05-21 11:08:47 +0900

    feat: Apply (게시글 지원) 모델 작성 완료

---
+++ models/Apply.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/Database");
+const Article = require("./Article");
+const User = require("./Users");
+const Qreply = require("./Qreply");
+
+const Apply = sequelize.define("Apply", {
+  apply_no: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user_no: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    references: {
+      model: User,
+      key: "user_no",
+    },
+  },
+  article_no: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    references: {
+      model: Article,
+      key: "article_no",
+    },
+  },
+  apply_result: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+});
+
+module.exports = Apply;
